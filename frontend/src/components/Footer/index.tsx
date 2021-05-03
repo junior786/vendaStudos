e60e94c40941a260a75fdbc496c42c857dd01030
junior786 <ricardo.jr1999@hotmail.com>
2021-05-03 14:41:55 -0300

    add componets

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/junior786" target="_blank" rel="noreferrer">Ricardo Fracassi</a></p>
+                <p className="text-light"><small><strong>Desenvolvido para estudos</strong><br /></small></p>
+              </div>
+    </footer>
+    );
+}
+export default Footer;
