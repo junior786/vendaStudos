5f7d8773b13e366cbfa8722d60cfd91ecf65d2a7
junior786 <ricardo.jr1999@hotmail.com>
2021-05-07 14:52:42 -0300

    bar chart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,12 @@
 import { type } from "node:os";
 
 export type SaleSum = {
-    sum: number,
-    sellerName: string
+    sum: number;
+    sellerName: string;
+}
+
+export type SaleSuccess = {
+    name: string;
+    visited: number;
+    deals: number;
 }
