800e455b85e3ad60d6dc8436f3dceb0310bec4c1
junior786 <ricardo.jr1999@hotmail.com>
2021-05-07 15:26:11 -0300

    data table integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,26 @@
-import { type } from "node:os";
+import { Seller } from "./seller";
+
+export type Sale = {
+    id: number,
+    visited: number,
+    deals: number,
+    amount: number,
+    date: string,
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    number: number;
+    size?: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+
+}
 
 export type SaleSum = {
     sum: number;
