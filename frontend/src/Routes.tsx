1d20441bb547a7dd3f8c91ad91a4b9d9b8ca8db4
junior786 <ricardo.jr1999@hotmail.com>
2021-05-07 13:53:27 -0300

    routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,22 @@
+import Dashboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+;
+const Routes = () => { // FUNÇÃO ANONIMA
+    return (
+        <BrowserRouter>
+        <Switch>
+            <Route path="/" exact>
+                <Home></Home>
+            </Route>
+
+            <Route path="/dashboard" exact>
+                <Dashboard/>
+            </Route>
+
+
+        </Switch>
+        </BrowserRouter>
+    );
+}
+export default Routes;
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => { // FUNÇÃO ANONIMA
+    return (
+        <div>
+            <div>
+                <h1>PAGINA HOME</h1>
+            </div>
+        </div>
+    );
+}
+export default Home;
